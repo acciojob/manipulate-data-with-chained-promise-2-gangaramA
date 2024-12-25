8ab877b59ccedda3b89d3732cfb87404dbb3087b
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-12-25 15:23:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
-// getData() function (unchanged)
+// getData() function
 function getData() {
   return new Promise((resolve) => {
     setTimeout(() => {
-      resolve([1, 2, 3, 4]); 
-    }, 3000); 
+      resolve([1, 2, 3, 4]);
+    }, 3000);
   });
 }
 
-// filterEven() function 
+// filterEven() function
 function filterEven(data) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const evenNumbers = data.filter(num => num % 2 === 0);
-      // Update the output element here
-      document.getElementById('output').textContent = evenNumbers.join(', '); 
+      const evenNumbers = data.filter((num) => num % 2 === 0);
+      document.getElementById('output').textContent = evenNumbers.join(', ');
       resolve(evenNumbers);
-    }, 1000); 
+    }, 1000);
   });
 }
 
@@ -23,11 +22,10 @@ function filterEven(data) {
 function multiplyByTwo(data) {
   return new Promise((resolve) => {
     setTimeout(() => {
-      const multipliedNumbers = data.map(num => num * 2);
-      // Update the output element here
-      document.getElementById('output').textContent = multipliedNumbers.join(', '); 
+      const multipliedNumbers = data.map((num) => num * 2);
+      document.getElementById('output').textContent = multipliedNumbers.join(', ');
       resolve(multipliedNumbers);
-    }, 2000); 
+    }, 2000);
   });
 }
 
@@ -35,22 +33,7 @@ function multiplyByTwo(data) {
 getData()
   .then(filterEven)
   .then(multiplyByTwo)
-  .catch(error => console.error(error));
-
-// Cypress test code
-describe('Data Manipulation with Promises', () => {
-  it('should filter even numbers and multiply them by 2', () => {
-    cy.visit('/main.html'); 
-
-    cy.get('#output').should('have.text', ''); 
-
-    // Increase wait time to allow filterEven() to complete
-    cy.wait(1500); 
-
-    cy.get('#output').should('have.text', '2,4'); 
-
-    cy.wait(2000); 
-
-    cy.get('#output').should('have.text', '4,8'); 
+  .catch((error) => {
+    console.error(error);
+    document.getElementById('output').textContent = 'An error occurred!';
   });
-});
